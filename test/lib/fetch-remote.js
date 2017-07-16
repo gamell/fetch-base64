969e3f26fcf69574f58554f81cf206cc0a150042
Joan Gamell <2460215+gamell@users.noreply.github.com>
2017-07-16 00:51:02 -0700

    Support fetching resources from https

--- test/lib/fetch-remote.js
+++ test/lib/fetch-remote.js
@@ -4,13 +4,14 @@ const assert = require('chai').assert;
 const fetchRemote = require('../../lib/fetch-remote.js');
 const sinon = require('sinon');
 const http = require('http');
-const url = require('url');
+const https = require('https');
+const legacyUrl = require('url');
 const PassThrough = require('stream').PassThrough;
 
 let sandbox;
 let request;
 let response;
-let httpRequestStub;
+let requestStub;
 
 function setupReqRes(statusCode) {
   request = new PassThrough();
@@ -20,9 +21,9 @@ function setupReqRes(statusCode) {
   response.end();
 }
 
-function setupSuccessfulResponseMock(sb) {
+function setupSuccessfulResponseMock(sb, agent) {
   setupReqRes(200);
-  httpRequestStub = sb.stub(http, 'request').callsArgWith(1, response).returns(request);
+  requestStub = sb.stub(agent, 'request').callsArgWith(1, response).returns(request);
 }
 
 function swallow() {}
@@ -41,26 +42,15 @@ describe('fetchRemote (Unit)', () => {
     assert(typeof remote2.then === 'function');
     Promise.all([remote1, remote2]).catch(swallow);
   });
-  it('should call url.parse with correct url', (done) => {
-    const urlStub = sandbox.stub(url, 'parse').callsFake(() => ({ prop1: 'value1' }));
-    // setup http.requests stubs
-    setupSuccessfulResponseMock(sandbox);
-    fetchRemote.fetch('http://url.com/existing-image.gif').then(() => {
-      assert(urlStub.calledOnce);
-      assert(urlStub.calledWith('http://url.com/existing-image.gif'));
-      done();
-    }).catch((e) => done(e));
-  });
   it('should call http.request with correct url and options', (done) => {
-    setupSuccessfulResponseMock(sandbox);
-    sandbox.stub(url, 'parse').callsFake(() => ({}));
+    setupSuccessfulResponseMock(sandbox, http);
     fetchRemote.fetch('http://url.com/existing-image.gif').then(() => {
       const expectedOptions = {
         headers: {},
         method: 'GET',
       };
-      assert(httpRequestStub.calledOnce);
-      sinon.assert.calledWith(httpRequestStub,
+      assert(requestStub.calledOnce);
+      sinon.assert.calledWith(requestStub,
         sinon.match(expectedOptions),
         sinon.match.typeOf('function')
       );
@@ -68,34 +58,50 @@ describe('fetchRemote (Unit)', () => {
     }).catch((e) => done(e));
   });
   it('should call url.resolve if several paths passed', (done) => {
-    setupSuccessfulResponseMock(sandbox);
-    const urlParseStub = sandbox.stub(url, 'parse').callsFake(() => ({}));
-    const urlResolveStub = sandbox.stub(url, 'resolve').callsFake(() => 'http://url.com/existing-image.gif');
+    setupSuccessfulResponseMock(sandbox, http);
+    const urlResolveStub = sandbox.stub(legacyUrl, 'resolve').callsFake(() => 'http://url.com/existing-image.gif');
     fetchRemote.fetch('http://url.com', '/existing-image.gif').then(() => {
       assert(urlResolveStub.calledOnce);
       sinon.assert.calledWith(urlResolveStub, 'http://url.com', '/existing-image.gif');
-      assert(urlParseStub.calledWith('http://url.com/existing-image.gif'));
       done();
     }).catch((e) => done(e));
   });
   it('should not call url.resolve if only one path passed', (done) => {
-    setupSuccessfulResponseMock(sandbox);
-    const urlParseStub = sandbox.stub(url, 'parse').callsFake(() => ({}));
-    const urlResolveStub = sandbox.stub(url, 'resolve').callsFake(() => ({}));
+    setupSuccessfulResponseMock(sandbox, http);
+    const urlResolveStub = sandbox.stub(legacyUrl, 'resolve').callsFake(() => ({}));
     fetchRemote.fetch('http://url.com/image.gif').then(() => {
       assert.equal(urlResolveStub.callCount, 0);
-      assert(urlParseStub.calledOnce);
-      sinon.assert.calledWith(urlParseStub, 'http://url.com/image.gif');
       done();
     }).catch((e) => done(e));
   });
-  describe('http.request', () => {
+  describe('http', () => {
+    it('should be called for http resources', (done) => {
+      setupSuccessfulResponseMock(sandbox, http);
+      fetchRemote.fetch('http://gamell.io/sprite.png').then(() => {
+        assert.equal(requestStub.callCount, 1);
+        done();
+      }).catch((e) => done(e));
+    });
     it('should return with expected error', (done) => {
-      setupReqRes(200);
-      sandbox.stub(url, 'parse').callsFake(() => ({}));
       const shouldNotBeCalled = sinon.spy();
       fetchRemote.fetch('http://127.0.0.1/existing-image.gif').then(shouldNotBeCalled, (error) => {
         assert.equal(error, 'HTTP Request error: Error: connect ECONNREFUSED 127.0.0.1:80');
+        done();
+      }).catch((e) => done(e));
+    });
+  });
+  describe('https', () => {
+    it('should be called for https resources', (done) => {
+      setupSuccessfulResponseMock(sandbox, https);
+      fetchRemote.fetch('https://gamell.io/sprite.png').then(() => {
+        assert.equal(requestStub.callCount, 1);
+        done();
+      }).catch((e) => done(e));
+    });
+    it('should return with expected error', (done) => {
+      const shouldNotBeCalled = sinon.spy();
+      fetchRemote.fetch('https://127.0.0.1/existing-image.gif').then(shouldNotBeCalled, (error) => {
+        assert.equal(error, 'HTTP Request error: Error: connect ECONNREFUSED 127.0.0.1:443');
         done();
       }).catch((e) => done(e));
     });
