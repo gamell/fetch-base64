c40b108bf018ada51fdf679481531740e30da8d4
Joan Gamell <gamell@gamell.io>
2018-07-09 23:25:15 -0700

    new api for fetch.remote() which accepts an options object (with headers) and tests

--- test/headers.js
+++ test/headers.js
@@ -6,9 +6,10 @@ const sinon = require('sinon');
 const server = require('./fixtures/server.js');
 
 let sandbox;
+const SERVER_PORT = 3456;
 
-describe('Headers tests', () => {
-  before(server.start);
+describe('Header tests', () => {
+  before(() => server.start(SERVER_PORT));
   after(server.stop);
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
@@ -18,12 +19,23 @@ describe('Headers tests', () => {
   });
 
   it('the headers should include UA', (done) => {
-    fetch.remote('http://localhost:3456').then(() => {
+    fetch.remote(`http://localhost:${SERVER_PORT}`).then(() => {
       const headers = server.getLastRequest().headers;
-      console.log(headers);
-      assert.include(headers, {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+      assert.include(headers, { // node always lower-cases all headers
+        'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
       });
+      assert.equal(Object.keys(headers).length, 3);
+      done();
+    }).catch(e => done(e));
+  });
+
+  it('it should include customized headers', (done) => {
+    fetch.remote({ url: `http://localhost:${SERVER_PORT}`, headers: { 'xxx-test': 'foo' } }).then(() => {
+      const headers = server.getLastRequest().headers;
+      assert.include(headers, { // node always lower-cases all headers
+        'xxx-test': 'foo'
+      });
+      assert.equal(Object.keys(headers).length, 4);
       done();
     }).catch(e => done(e));
   });
